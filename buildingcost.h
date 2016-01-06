5e83acebccfeff58967fab9c5bd3ec8b4cf03ae5
Marcin Jozwik <marty.jozwik@gmail.com>
2016-01-06 17:38:02 +0100

    dodana class buildingcost

---
+++ buildingcost.h
@@ -0,0 +1,17 @@
+#ifndef BUILDINGCOST_H
+#define BUILDINGCOST_H
+
+#include "goods.h"
+
+class BuildingCost
+{
+    public:
+    static  Goods sawmillCost;
+    static  Goods stonecutterhutCost;
+    static  Goods barracksCost;
+    static  Goods residenceCost;
+    static  Goods goldmineCost;
+    static  Goods farmCost;
+};
+
+#endif // BUILDINGCOST_H
